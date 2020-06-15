e7689618e661333eed5fd79ce510d88ce4be2fc8
toshiyanishimura1963 <toshiya.nishimura@gmail.com>
2020-06-15 17:58:54 +0900

    20200615 不要なダイアログ対応

--- auction_allresale.py
+++ auction_allresale.py
@@ -64,7 +64,6 @@ def sendkeys_element(element_type, element_name, send_strings):
     else:
         print("sendkeys_element error")
         exit()
-      
 
 
 inifile = configparser.ConfigParser()
@@ -86,10 +85,8 @@ driver.get("https://auctions.yahoo.co.jp")
 
 # 不要なダイアログが表示されたときは閉じる
 if "display: block" in driver.find_element_by_id('js-prMdl-sbym').get_attribute("style"):
-    # click_element("xpath", "//a[@class='prMdl__close']")
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     driver.find_element_by_class_name('prMdl__close').click()
-    print("test")
 
 if len(driver.find_elements_by_link_text("ログイン")) > 0:
     click_element("link_text", "ログイン")
@@ -102,14 +99,19 @@ click_element("id", "btnSubmit")
 if len(driver.find_elements_by_link_text("ご利用中のサービスに戻る")) > 0:
     click_element("link_text", "ご利用中のサービスに戻る")
 
+# 不要なダイアログが表示されたときは閉じる
+if "display: block" in driver.find_element_by_id('js-prMdl').get_attribute("style"):
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    driver.find_element_by_id('js-prMdl-close').click()
+
 click_element('link_text', 'マイオク')
 click_element('link_text', '出品終了分')
 click_element('link_text', '落札者なし')
 
-
-# get all the href attributes
 # ダイアログの問い合わせステータス
 display_dialog1 = "yes"
+WebDriverWait(driver, 30).until(
+        EC.element_to_be_clickable((By.LINK_TEXT, '落札者あり')))
 while len(driver.find_elements_by_xpath("//img[@alt='再出品']")) > 0:
     print("再出品する商品の数: " + str(len(driver.find_elements_by_xpath("//img[@alt='再出品']"))))
     # 不要なダイアログの対応 落札者なしのリスト
@@ -121,14 +123,6 @@ while len(driver.find_elements_by_xpath("//img[@alt='再出品']")) > 0:
             display_dialog1 = "no"
             driver.find_element_by_class_name('closeBtn').click()
     click_element('xpath', "//img[@alt='再出品']")
-    # click_element('link_text', Shohin)
-
-    # # 返品を受け付けるにチェックがはいっていない場合は、チェックする
-    # WebDriverWait(driver, 30).until(
-    #     EC.presence_of_element_located((By.XPATH, '//*[@id="FormReqrd"]/div[13]/input')))
-    # if driver.find_element_by_xpath('//*[@id="FormReqrd"]/div[13]/input').get_attribute('value') == "0":
-    #     print('返品可をチェックする')
-    #     click_element("xpath", '//*[@id="FormReqrd"]/div[13]/label/span[1]')
 
     # 値下げ交渉はしないので、値下げ交渉のチェックを外す
     if 'is-check' in driver.find_element_by_xpath('//*[@id="price_buynow"]/div[3]/label').get_attribute('class'):
@@ -136,56 +130,9 @@ while len(driver.find_elements_by_xpath("//img[@alt='再出品']")) > 0:
         click_element("xpath",
                       '//*[@id="price_buynow"]/div[3]/label/span[1]')
 
-    # if len(driver.find_elements_by_link_text("HTMLタグ入力")) > 0:
-    #     click_element("link_text", "HTMLタグ入力")
-    #     print('HTMLタグ入力')
-
-    # # '出品者が送料負担
-    # click_element("xpath",
-    #               '//*[@id="FormReqrd"]/section[2]/div[6]/label[1]')
-    # print("出品者が送料負担")
-
-    # # '1から2日で発送
-    # click_element("xpath",
-    #               '//*[@id="standardDeliveryArea"]/div[2]/label[1]')
-    # print("1から2日で発送")
-
-    # 商品の状態を入力
-    # Select(driver.find_element_by_name("istatus")).select_by_value('new')
-    # print("商品の状態を入力")
-
-     # ゆうパケットお手軽版
-    # click_element("xpath", '//*[@id="yubinForm"]/div[2]/ul/li[1]')
-    # print("ゆうパケットお手軽版")
-
     # 終了日を入力
     Select(driver.find_element_by_name("ClosingYMD")).select_by_index(6)
     print("終了日を入力")
-
-    # # セールスモード
-    # click_element(
-    #     "xpath", '//*[@id="FormReqrd"]/section[4]/div[3]/div[2]/label[2]')
-    # print("セールスモード:フリマ")
-
-    # # 価格のクリア
-    # driver.find_element_by_id(
-    #     'auc_BidOrBuyPrice_buynow').clear()
-    # print("価格のクリア")
-    # # 価格の入力
-    # sendkeys_element("id",
-    #                  'auc_BidOrBuyPrice_buynow', price)
-    # print("価格の入力")
-
-    # # 商品説明のクリア CTRL+A, DELETE
-    # driver.find_element_by_id(
-    #     "rteEditorComposition0").send_keys(Keys.CONTROL, "a")
-    # driver.find_element_by_id(
-    #     "rteEditorComposition0").send_keys(Keys.DELETE)
-    # for line in description.split('\\n'):
-    #     driver.find_element_by_id("rteEditorComposition0").send_keys(line)
-    #     driver.find_element_by_id(
-    #         "rteEditorComposition0").send_keys(Keys.ENTER)
-    # print("商品説明")
 
     # '出品するボタン
     if debug_on:
