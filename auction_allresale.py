75294835dd38e6c25333fd92d04dd0d14ccba9ef
Toshiya Nishimura <toshiya.nishimura@gmail.com>
2021-08-26 23:40:39 +0900

    UserData処理のダブリ解除

--- auction_allresale.py
+++ auction_allresale.py
@@ -77,12 +77,6 @@ debug_on = inifile.get('設定', 'debug') == "ON"
 
 userdata_dir = 'UserData'  # カレントディレクトリの直下に作る場合
 os.makedirs(userdata_dir, exist_ok=True)
-options = webdriver.ChromeOptions()
-options.add_argument('--user-data-dir=' + userdata_dir)
-driver = webdriver.Chrome(options=options)
-
-userdata_dir = 'UserData'  # カレントディレクトリの直下に作る場合
-os.makedirs(userdata_dir, exist_ok=True)
 cwdpath = os.getcwd()
 options = webdriver.ChromeOptions()
 options.add_argument('--user-data-dir=' + cwdpath + "/" + userdata_dir)
